e30f3b9e79dbaf4c9ea1163ef6fc6736712e6bea
anfo270 <andres44chuy@gmail.com>
2024-03-27 13:00:13 -0600

    redireccion de inicio a login

--- Desponchadora/src/app/app-routing.module.ts
+++ Desponchadora/src/app/app-routing.module.ts
@@ -2,22 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'registro-login',
-    loadChildren: () => import('./registro-login/registro-login.module').then( m => m.RegistroLoginPageModule)
+    loadChildren: () => import('./registro-login/registro-login.module').then(m => m.RegistroLoginPageModule)
   },
 ];
 
