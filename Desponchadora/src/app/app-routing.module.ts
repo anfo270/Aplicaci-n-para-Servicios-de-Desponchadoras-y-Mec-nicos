be37a6985a96491b8ed2ba824123f053de260bb0
DM <147567713+DoryanXS@users.noreply.github.com>
2024-04-09 09:07:07 -0700

    Pantalla de carga establecida

--- Desponchadora/src/app/app-routing.module.ts
+++ Desponchadora/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashScreenComponent },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
@@ -23,13 +21,46 @@ const routes: Routes = [
     path: 'productos',
     loadChildren: () => import('./productos/productos.module').then(m => m.ProductosPageModule)
   },
-
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//import { NgModule } from '@angular/core';
+//import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+//const routes: Routes = [
+//  {
+//    path: '',
+//    redirectTo: 'login',
+//    pathMatch: 'full'
+//  },
+//  {
+//    path: 'login',
+//    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+//  },
+//  {
+//    path: 'registro-login',
+//    loadChildren: () => import('./registro-login/registro-login.module').then(m => m.RegistroLoginPageModule)
+//  },
+//  {
+//    path: 'negocio',
+//    loadChildren: () => import('./negocio/negocio.module').then(m => m.NegocioPageModule)
+//  },
+//  {
+//    path: 'productos',
+//    loadChildren: () => import('./productos/productos.module').then(m => m.ProductosPageModule)
+//  },
+
+//];
+
+//@NgModule({
+//  imports: [
+//    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+//  ],
+//  exports: [RouterModule]
+//})
+//export class AppRoutingModule { }
