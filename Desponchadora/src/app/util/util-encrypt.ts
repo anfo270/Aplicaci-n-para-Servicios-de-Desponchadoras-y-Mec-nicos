f246dd752bf43a165eb518f869f0b7eeb4c14258
anfo270 <andres44chuy@gmail.com>
2024-04-10 08:31:58 -0600

    Correcion de criptacion y descriptacion

--- Desponchadora/src/app/util/util-encrypt.ts
+++ Desponchadora/src/app/util/util-encrypt.ts
@@ -1,22 +1,12 @@
 import * as CryptoJS from 'crypto-js';
 
-export const encrypt = (data: string, key: string): string => {
-    const keyBytes = CryptoJS.enc.Utf8.parse(key);
-    const iv = CryptoJS.lib.WordArray.random(16); // IV de 128 bits (16 bytes)
-
-    const encrypted = CryptoJS.AES.encrypt(data, keyBytes, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-    });
-
-    // Concatenar el IV con el texto cifrado para que pueda ser utilizado en el proceso de descifrado
-    const cipherTextWithIV = iv.concat(encrypted.ciphertext);
-
-    // Convertir el texto cifrado (incluyendo el IV) a una representación en Base64 para su almacenamiento o transmisión
-    return CryptoJS.enc.Base64.stringify(cipherTextWithIV);
+export const encrypt = (data: string): string => {
+    const encryptedData = CryptoJS.AES.encrypt(data, 'asd123');
+    return encryptedData.toString();
 };
 
-// Ejemplo de uso
-const encryptedData = encrypt('Hola Mundo', '1234567890123456');
-console.log('Texto cifrado:', encryptedData);
+export const dencrypt = (data: string): string => {
+    const decryptedData = CryptoJS.AES.decrypt(data, 'asd123').toString(CryptoJS.enc.Utf8);
+    return decryptedData;
+}
+
